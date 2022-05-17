c4897b37124f76a50aeef9c5ef023c54bef9b1d4
Riccardo Gulin <riccardo@nucleode.com>
2022-05-17 10:26:38 +0200

    Adds CORS options

--- src/server.js
+++ src/server.js
@@ -7,10 +7,11 @@ import usersRouter from "./apis/users/index.js"
 import booksRouter from "./apis/books/index.js"
 import filesRouter from "./apis/files/index.js"
 import { genericErrorHandler, notFoundErrorHandler, badRequestErrorHandler, unauthorizedErrorHandler } from "./errorHandlers.js"
+import createError from "http-errors"
 
 const server = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 
 const publicFolderPath = join(process.cwd(), "./public")
 
@@ -26,8 +27,39 @@ const anotherMiddleware = (req, res, next) => {
   next()
 }
 
+// ************************************** CORS **************************************************
+
+/* CROSS ORIGIN RESOURCE SHARING
+
+ORIGIN:
+
+1. FE=http://localhost:3000 and BE=http://localhost:3001 <-- 2 different port numbers they represent different origins
+2. FE=https://mywonderfulfe.com and BE =https://mywonderfulbe.com <-- 2 different domains they represent different origins
+3. FE=http://domain.com and BE https://domain.com <-- 2 different protocols they represent different origins
+- 
+
+*/
+
+// **********************************************************************************************
+const whitelist = ["http://localhost:3000", "https://mywonderfulfe.com"]
+
+const corsOptions = {
+  origin: (origin, next) => {
+    // cors is a global middleware --> for each and every request we are going to be able to read the current origin value
+    console.log("CURRENT ORIGIN: ", origin)
+
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      // origin is in the whitelist --> move next with no errors
+      next(null, true)
+    } else {
+      // origin is NOT in the whitelist --> trigger an error
+      next(createError(400, `Cors Error! your origin ${origin} is not in the list!`))
+    }
+  },
+}
+
 server.use(express.static(publicFolderPath))
-server.use(cors()) // GLOBAL MIDDLEWARE to connect FE and BE without errors in the browser
+server.use(cors(corsOptions)) // GLOBAL MIDDLEWARE to connect FE and BE without errors in the browser
 server.use(loggerMiddleware) // GLOBAL MIDDLEWARE
 server.use(express.json()) // GLOBAL MIDDLEWARE if you don't add this BEFORE the endpoints all requests' bodies will be UNDEFINED
 
@@ -48,5 +80,3 @@ server.listen(port, () => {
   console.table(listEndpoints(server))
   console.log(`Server is listening on port ${port}!`)
 })
-
-console.log(process.env.MONGO_CONNECTION_STRING)
