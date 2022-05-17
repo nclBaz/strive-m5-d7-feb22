4609651a84f28976b3e8e1b27a70ec1a438c9561
Riccardo Gulin <riccardo@nucleode.com>
2022-05-17 12:44:12 +0200

    Adds Cloudinary

--- src/apis/files/index.js
+++ src/apis/files/index.js
@@ -1,39 +1,44 @@
 import express from "express"
 import multer from "multer"
 import createError from "http-errors"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
 
 import { saveUsersAvatars } from "../../lib/fs-tools.js"
 
 const filesRouter = express.Router()
 
-filesRouter.post(
-  "/:userId/avatar",
-  multer({
-    fileFilter: (req, file, multerNext) => {
-      if (file.mimetype !== "image/gif") {
-        multerNext(createError(400, "Only GIF allowed!"))
-      } else {
-        multerNext(null, true)
-      }
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary, // this searches in your process.env for something called CLOUDINARY_URL, which contains your API environment variable
+    params: {
+      folder: "feb22/books",
     },
-    limits: { fileSize: 1 * 1024 },
-  }).single("avatar"),
-  async (req, res, next) => {
-    // "avatar" needs to match exactly to the field name appended to the FormData object in the FE, otherwise Multer is not going to be able to find the file into the request body
-    try {
-      console.log("FILE: ", req.file)
-      await saveUsersAvatars(req.file.originalname, req.file.buffer)
-
-      // find user by userId in users.json
-
-      // update avatar field of that user adding "/img/users/3kgdm0l30101f5.gif"
-      // in FE "http://localhost:3001" + "/img/users/3kgdm0l30101f5.gif"
-      res.send()
-    } catch (error) {
-      next(error)
+  }),
+  fileFilter: (req, file, multerNext) => {
+    if (file.mimetype !== "image/gif") {
+      multerNext(createError(400, "Only GIF allowed!"))
+    } else {
+      multerNext(null, true)
     }
+  },
+  limits: { fileSize: 1 * 1024 * 1024 },
+}).single("avatar")
+
+filesRouter.post("/:userId/avatar", cloudinaryUploader, async (req, res, next) => {
+  // "avatar" needs to match exactly to the field name appended to the FormData object in the FE, otherwise Multer is not going to be able to find the file into the request body
+  try {
+    console.log("FILE: ", req.file)
+
+    // find user by userId in users.json
+
+    // update avatar field of that user adding "https://res.cloudinary.com/riccardostrive/image/upload/v1652783597/feb22/books/sxmhy9wwa0xukoumv8ur.gif"
+    // in FE "https://res.cloudinary.com/riccardostrive/image/upload/v1652783597/feb22/books/sxmhy9wwa0xukoumv8ur.gif"
+    res.send()
+  } catch (error) {
+    next(error)
   }
-)
+})
 
 filesRouter.post("/multipleUpload", multer().array("avatars"), async (req, res, next) => {
   try {
